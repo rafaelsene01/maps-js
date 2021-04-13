f042708b640a7153ff6e33fcf5a6a28b31c9c037
rafaelsene01 <rafaelssene@gmail.com>
2021-04-13 11:40:39 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,375 @@
+<template>
+  <div class="flex h-full">
+    <div class="w-1/2 h-full">
+      <div id="map" class="w-full h-full" />
+    </div>
+    <div class="w-1/2 h-full">
+      <div class="bg-green-200">
+        <div>
+          <label>Title</label>
+          <p>{{ Information.title }}</p>
+        </div>
+        <div>
+          <label>Nome</label>
+          <p>{{ Information.farmName }}</p>
+        </div>
+        <div class="flex">
+          <div class="w-1/3">
+            <label>NIRF</label>
+            <p>{{ Information.nirf }}</p>
+          </div>
+          <div class="w-1/3">
+            <label>Núm. INCRA</label>
+            <p>{{ Information.incra }}</p>
+          </div>
+          <div class="w-1/3">
+            <label>Área Total</label>
+            <p>{{ Information.areaTotal }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="bg-yellow-200 mt-2">
+        <div class="flex" v-for="owner in Owners" :key="owner.taxId">
+          <div class="w-1/3">
+            <label>CPF/CNPJ</label>
+            <p>{{ owner.taxId }}</p>
+          </div>
+          <div class="w-1/3">
+            <label>Nome</label>
+            <p>{{ owner.name }}</p>
+          </div>
+          <div class="w-1/3">
+            <label>%</label>
+            <p>{{ owner.share }}</p>
+          </div>
+        </div>
+      </div>
+      {{ index }} <span @click="log()"> Console </span>
+      <div class="bg-red-200">
+        <div class="flex">
+          <div class="w-1/4">
+            <label>Matrícula</label>
+          </div>
+          <div class="w-full">
+            <label>Nome</label>
+          </div>
+          <div class="w-1/5">
+            <label>Todo</label>
+          </div>
+        </div>
+
+        <div class="flex" v-for="(item, index) in IncraData" :key="index">
+          <div class="w-1/4">
+            <p>{{ item.registry }}</p>
+          </div>
+          <div class="w-full">
+            <p>{{ item.farmName }}</p>
+          </div>
+          <div class="w-1/5 flex">
+            <p class="mr-2" @click="focusMapIndex(index)">Focar</p>
+            <button @click="focusMapIndex(index, true)">ED</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import DataJson from "@/assets/data";
+import { randHexadecimalColor, invertLatLong } from "@/assets/farmLib";
+
+export default defineComponent({
+  name: "App",
+
+  // data(): any {
+  //   return {
+  //     Farm: {},
+  //     MapsLib: {},
+  //     Map: {},
+  //     status: "Buscando dados",
+  //     index: -1,
+  //   };
+  // },
+  data: () => ({
+    Farm: {},
+    hasMapsLib: false,
+    Map: {},
+    status: "Buscando dados",
+    index: -1,
+  }),
+
+  beforeCreate() {
+    if (!(window as any)?.google?.maps) {
+      const googleMapsKey = "AIzaSyAkRnzcWdZfy9PQnI0d_Zp1CkKFkSin82U";
+      const googleMaps = document.createElement("script");
+      googleMaps.setAttribute(
+        "src",
+        `https://maps.googleapis.com/maps/api/js?key=${googleMapsKey}&libraries=drawing`
+      );
+      document.head.appendChild(googleMaps);
+    }
+  },
+
+  created() {
+    this.Farm = DataJson();
+  },
+
+  mounted() {
+    if (this.IncraData.length > 0) {
+      if (!(window as any).google?.maps) {
+        setTimeout(() => {
+          if ((window as any).google?.maps) {
+            this.hasMapsLib = true;
+            this.initMap();
+          }
+        }, 1000);
+      } else {
+        this.initMap();
+      }
+    }
+  },
+
+  methods: {
+    log() {
+      console.log(this.Map);
+    },
+    initMap() {
+      const maps = (window as any).google?.maps;
+
+      maps.Polygon.prototype.getBounds = function () {
+        let bounds = new maps.LatLngBounds();
+        let paths = this.getPaths();
+        let path;
+        for (let i = 0; i < paths.getLength(); i++) {
+          path = paths.getAt(i);
+          for (let ii = 0; ii < path.getLength(); ii++) {
+            bounds.extend(path.getAt(ii));
+          }
+        }
+        return bounds;
+      };
+
+      // this.Map.addListener("click", (e) => {
+      //   console.log(e);
+      // });
+    },
+
+    changeIndex(value: number) {
+      this.index = value;
+    },
+
+    googleGeoData(data) {
+      return data.reduce((arr, item) => {
+        return [...arr, { lat: item[0], lng: item[1] }];
+      }, []);
+    },
+
+    setMap(map, centerPolygon, index?) {
+      const maps = (window as any).google?.maps;
+
+      // const drawing = new maps.drawing.DrawingManager({
+      //   map,
+      //   drawingMode: maps.drawing.OverlayType.POLYGON,
+      //   drawingControl: true,
+      //   drawingControlOptions: {
+      //     position: maps.ControlPosition.TOP_CENTER,
+      //     drawingModes: ["polygon"],
+      //   },
+
+      //   polygonOptions: {
+      //     strokeColor: "#0f0",
+      //     editable: true,
+      //     dragabble: true,
+      //   },
+      // });
+
+      // drawing.addListener("polygoncomplete", (e) => {
+      //   console.log(e);
+      //   e.setEditable(true);
+
+      //   drawing.setDrawingMode();
+      // });
+
+      map.fitBounds(centerPolygon.getBounds());
+
+      this.IncraData.forEach((item, ind) => {
+        const color =
+          index === undefined
+            ? randHexadecimalColor()
+            : index === ind
+            ? "#0f0"
+            : "#999";
+        for (const latLngs of item.latlngs) {
+          const newPolygon = new maps.Polygon({
+            paths: this.googleGeoData(latLngs),
+            strokeColor: color,
+          });
+          if (index !== ind) {
+            newPolygon.setMap(map);
+          }
+        }
+      });
+
+      if (index > -1) {
+        for (const latLngs of this.IncraData[index].latlngs) {
+          const newPolygon = new maps.Polygon({
+            paths: this.googleGeoData(latLngs),
+            strokeColor: "#0f0",
+            editable: true,
+          });
+
+          newPolygon.setMap(map);
+
+          newPolygon.addListener("drag", function (polygon) {
+            console.log(polygon);
+          });
+        }
+      }
+    },
+
+    focusMap() {
+      const maps = (window as any).google?.maps;
+
+      this.changeIndex(-1);
+
+      const polygonArray = this.geoData.map((item) => {
+        return new maps.LatLng(item[0], item[1]);
+      });
+
+      const Polygon = new maps.Polygon({
+        paths: polygonArray,
+      });
+
+      this.Map = new maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: {
+          lat: -12.726084,
+          lng: -47.532103,
+        },
+        zoomControl: true,
+        mapTypeControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        rotateControl: false,
+        fullscreenControl: true,
+        disableDefaultUI: false,
+        mapTypeId: "hybrid",
+      });
+
+      this.setMap(this.Map, Polygon);
+    },
+
+    focusMapIndex(index) {
+      const maps = (window as any).google?.maps;
+
+      this.changeIndex(index);
+
+      const poly: any = [];
+
+      for (const latLngs of this.IncraData[index].latlngs) {
+        for (const item of latLngs) {
+          poly.push(item);
+        }
+      }
+
+      const polygonArray = poly.map((item) => {
+        return new maps.LatLng(item[0], item[1]);
+      });
+
+      const selectPolygon = new maps.Polygon({
+        paths: polygonArray,
+      });
+
+      this.Map = new maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: {
+          lat: -12.726084,
+          lng: -47.532103,
+        },
+        zoomControl: true,
+        mapTypeControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        rotateControl: false,
+        fullscreenControl: true,
+        disableDefaultUI: false,
+        mapTypeId: "hybrid",
+      });
+
+      this.setMap(this.Map, selectPolygon, index);
+    },
+  },
+
+  computed: {
+    Information(): any {
+      return {
+        title: this.Farm?.farmName,
+        farmName: this.Farm?.farmData?.farmName || "",
+        areaTotal:
+          this.Farm.farmData?.totalArea || this.Farm.declaredTotalArea || "-",
+        incra: this.Farm?.farmData?.numIncra || this.Farm.incraNumber || "-",
+        nirf: this.Farm?.farmData?.nirf || this.Farm.nirfNumber || "-",
+      };
+    },
+    Owners(): any {
+      return this.Farm?.farmData?.owners?.reduce((items, item) => {
+        let shareholders = [];
+        if (item.shareholders) {
+          shareholders = item.shareholders?.reduce((its, it) => {
+            return [
+              ...its,
+              {
+                taxId: it.ni,
+                share: it.percentualParticipacao,
+              },
+            ];
+          }, []);
+        }
+        return [
+          ...items,
+          {
+            taxId: item.taxId,
+            name: item.name,
+            share: item.share,
+          },
+          ...shareholders,
+        ];
+      }, []);
+    },
+    IncraData(): any {
+      return this.Farm.incraData.map((item, index) => {
+        return {
+          ...item,
+          index,
+          latlngs:
+            !!item?.geometry?.coordinates && item?.geometry.type === "Polygon"
+              ? invertLatLong(item?.geometry?.coordinates)
+              : [],
+        };
+      });
+    },
+    geoData(): any {
+      return this.IncraData.reduce((arr, item) => {
+        return [
+          ...arr,
+          ...item?.latlngs?.reduce((ar, it) => {
+            return [...ar, ...it];
+          }, []),
+        ];
+      }, []);
+    },
+  },
+
+  watch: {
+    hasMapsLib(value) {
+      if (value) {
+        this.focusMap();
+      }
+    },
+  },
+});
+</script>
+
+<style lang="postcss" scoped></style>
---
+++ src/assets/farmLib.js
@@ -0,0 +1,47 @@
+function shuffle(array) {
+  let instance = array.length,
+    aux_value,
+    index_rand;
+
+  while (0 !== instance) {
+    index_rand = Math.floor(Math.random() * instance);
+    instance -= 1;
+
+    aux_value = array[instance];
+    array[instance] = array[index_rand];
+    array[index_rand] = aux_value;
+  }
+
+  return array;
+}
+
+export const randHexadecimalColor = () => {
+  return `#${shuffle([
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+  ])
+    .slice(0, 6)
+    .join("")}`;
+};
+
+export const invertLatLong = (data) => {
+  return data.map((arr) => {
+    return arr.map((item) => {
+      return [item[1], item[0]];
+    });
+  });
+};
